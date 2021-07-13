07f4ecaf0298b85f42375e6c0ab002490786d711
ivannotaloka <49152775+ivannotaloka@users.noreply.github.com>
2021-07-13 19:44:50 +0200

    primer cambio pagina

--- parte1.js
+++ parte1.js
@@ -1,2 +1,12 @@
-const miTitulo = document.querySelector('h1');
-miTitulo.textContent = '¡Hola mundo!';
+/*const miTitulo = document.querySelector('h1');
+miTitulo.textContent = '¡Hola mundo!';*/
+
+let miImage = document.querySelector('img');
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/firefox-icon.png') {
+      miImage.setAttribute('src','images/image2.png');
+    } else {
+      miImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
